6f57c5dc9f8d554eb101466c688298e536b642e0
Claudia <clau13052002@gmail.com>
2022-10-24 16:24:41 -0500

    UI

---
+++ Hearthstone/app/src/main/java/com/example/hearthstone/ui/cardOverview/CardOverviewFragment.kt
@@ -0,0 +1,30 @@
+package com.example.hearthstone.ui.cardOverview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.hearthstone.R
+import com.example.hearthstone.databinding.FragmentCardOverviewBinding
+
+
+class CardOverviewFragment : Fragment() {
+    private lateinit var binding: FragmentCardOverviewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_card_overview, container, false)
+        return binding.root
+    }
+
+}
