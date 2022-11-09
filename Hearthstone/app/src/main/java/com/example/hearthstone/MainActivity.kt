2efa0f9eec04118dc830885f068c8c0e36c4dedc
Claudia <clau13052002@gmail.com>
2022-11-09 14:39:39 -0600

    I added the UI, api and permission

--- Hearthstone/app/src/main/java/com/example/hearthstone/MainActivity.kt
+++ Hearthstone/app/src/main/java/com/example/hearthstone/MainActivity.kt
@@ -4,26 +4,18 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
-import com.example.hearthstone.databinding.ActivityMainBinding
-import com.example.hearthstone.databinding.FragmentFavoritesBinding
+import com.example.hearthstone.ui.shopPage.ShopFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    //private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         val navHostFragment =
@@ -31,16 +23,20 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         // Setup the bottom navigation view with navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
         bottomNavigationView.setOnItemSelectedListener { item ->
             Log.d("Clau", "${item.itemId}")
             when(item.itemId) {
                 R.id.bottom_av_homeFragment -> {
                     navController.navigate(R.id.homeFragment)
-
                     true
                 }
                 R.id.bottom_av_favoritesFragment -> {
                     navController.navigate(R.id.favoritesFragment)
+                    true
+                }
+                R.id.bottom_av_shopFragment -> {
+                    navController.navigate(R.id.shopFragment)
                     true
                 }
                 else -> false
@@ -48,7 +44,6 @@ class MainActivity : AppCompatActivity() {
         }
         ourApplication = this.application
     }
-
     companion object {
         lateinit var ourApplication: Application
     }
